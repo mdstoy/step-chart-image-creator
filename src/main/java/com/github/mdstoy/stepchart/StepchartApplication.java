9f5a2bf74f35179307b2c83f41d48669185b09ee
mdstoy <toyofyou.me@gmail.com>
2018-05-17 00:06:45 +0900

    initial commit

---
+++ src/main/java/com/github/mdstoy/stepchart/StepchartApplication.java
@@ -0,0 +1,12 @@
+package com.github.mdstoy.stepchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepchartApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/mdstoy/stepchart/StepchartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.mdstoy.stepchart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StepchartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
