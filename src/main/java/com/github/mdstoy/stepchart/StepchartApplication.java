79536193abff65539c3438c1a9a66f50b01016b3
mdstoy <toyofyou.me@gmail.com>
2018-05-18 00:38:39 +0900

    use arguments

--- src/main/java/com/github/mdstoy/stepchart/StepchartApplication.java
+++ src/main/java/com/github/mdstoy/stepchart/StepchartApplication.java
@@ -2,11 +2,26 @@ package com.github.mdstoy.stepchart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class StepchartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StepchartApplication.class, args);
-	}
+    public static void main(String[] args) {
+	    try(ConfigurableApplicationContext context = SpringApplication.run(StepchartApplication.class, args)) {
+	        StepchartApplication application = context.getBean(StepchartApplication.class);
+	        application.run(args);
+        } catch (Exception e) {
+	        // FIXME
+	        e.printStackTrace();
+        }
+    }
+
+    public void run(String[] args) {
+        System.out.println("args:");
+        Arrays.stream(args).forEach(System.out::println);
+    }
 }
