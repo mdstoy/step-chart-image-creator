1ea7883ce90d7033db317cf77d3b66ef676bc3a8
mdstoy <toyofyou.me@gmail.com>
2018-05-29 00:33:35 +0900

    add arrow model

--- src/main/java/com/github/mdstoy/stepchart/ArrowContainer.java
+++ src/main/java/com/github/mdstoy/stepchart/ArrowContainer.java
@@ -1,22 +1,35 @@
 package com.github.mdstoy.stepchart;
 
+import com.github.mdstoy.stepchart.config.ArrowConfiguration;
 import com.github.mdstoy.stepchart.model.chart.ArrowAttribute;
+import com.github.mdstoy.stepchart.model.object.Arrow;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class ArrowContainer {
 
-    private Map<ArrowAttribute, BufferedImage> arrows;
+    private Map<ArrowAttribute, Arrow> arrows = new HashMap<>();
 
-    public ArrowContainer() {
-        arrows = new HashMap<>();
+    private ArrowConfiguration arrowConfig;
+
+    @Autowired
+    public ArrowContainer(ArrowConfiguration arrowConfig) throws IOException{
+        this.arrowConfig = arrowConfig;
+        setup();
     }
 
-    public BufferedImage getArrow(ArrowAttribute attribute) {
+    // FIXME : これじゃない
+    private void setup() throws IOException{
+        Arrow arrow = Arrow.of(arrowConfig.getImagePath());
+        arrows.put(null, arrow);
+    }
+
+    public Arrow getArrow(ArrowAttribute attribute) {
         return arrows.get(attribute);
     }
 }
---
+++ src/main/java/com/github/mdstoy/stepchart/model/object/Arrow.java
@@ -0,0 +1,58 @@
+package com.github.mdstoy.stepchart.model.object;
+
+import org.springframework.core.io.ClassPathResource;
+
+import javax.imageio.ImageIO;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Arrow {
+
+    BufferedImage image;
+
+    private Arrow(String imagePath) throws IOException{
+        try {
+            this.image = ImageIO.read(new ClassPathResource(imagePath).getFile());
+        } catch(IOException ioe) {
+            throw new IOException(String.format("failed create image. [%s]", imagePath), ioe);
+        }
+
+    }
+
+    public static Arrow of(String imagePath) throws IOException{
+        return new Arrow(imagePath);
+    }
+
+    // FIXME : インスタンスの扱い
+    public BufferedImage changeColor(int r, int g, int b) {
+        for(int i = 0; i < image.getHeight(); ++i) {
+            for(int j = 0; j < image.getWidth(); ++j) {
+                if (image.getRGB(i, j) == -16777216) {
+                    image.setRGB(i, j, (255 << 24) + (r << 16) + (g << 8) + b);
+                } else {
+                    image.setRGB(i, j, 0);
+                }
+            }
+        }
+
+        return image;
+    }
+
+    public BufferedImage rotate(int angle) {
+        // 変換の設定
+        AffineTransform at = new AffineTransform();
+        // TODO : 24d は取ってくる？
+        at.setToRotation(Math.toRadians(angle), 24d, 24d);
+        // 変換の実操作
+        AffineTransformOp op = new AffineTransformOp(at, AffineTransformOp.TYPE_BICUBIC);
+
+        // 書き出し用
+        // TODO : 48 は取ってくる
+        BufferedImage newImage = new BufferedImage(48, 48, BufferedImage.TYPE_INT_ARGB);
+        op.filter(image, newImage);
+
+        return newImage;
+    }
+}
