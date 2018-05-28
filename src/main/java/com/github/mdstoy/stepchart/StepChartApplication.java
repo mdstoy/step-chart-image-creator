5a9fe686e3bfca69dbd8de9454a493e805ffbe3a
mdstoy <toyofyou.me@gmail.com>
2018-05-28 23:07:13 +0900

    separate into use case from application

--- src/main/java/com/github/mdstoy/stepchart/StepChartApplication.java
+++ src/main/java/com/github/mdstoy/stepchart/StepChartApplication.java
@@ -1,46 +1,19 @@
 package com.github.mdstoy.stepchart;
 
-import com.github.mdstoy.stepchart.model.chart.StepChart;
+import com.github.mdstoy.stepchart.usecase.PartialInput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @SpringBootApplication
 public class StepChartApplication {
 
     public static void main(String[] args) {
 	    try(ConfigurableApplicationContext context = SpringApplication.run(StepChartApplication.class, args)) {
-            StepChartApplication stepChart = context.getBean(StepChartApplication.class);
-	        stepChart.run(args);
+            PartialInput bean = context.getBean(PartialInput.class);
+	        bean.run(args);
         } catch (Exception e) {
 	        System.err.println(e.getMessage());
-        }
-    }
-
-    public void run(String[] args) throws FileNotFoundException {
-
-        if (args.length != 1) {
-            throw new IndexOutOfBoundsException("Required only one option is file path.");
-        }
-
-        Path path = Paths.get(args[0]);
-
-        if (Files.notExists(path)) {
-            throw new FileNotFoundException(String.format("File not found [%s]", path.toString()));
-        }
-
-        try {
-            StepChart stepChart = StepChart.of(path);
-            System.out.println(stepChart);
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
---
+++ src/main/java/com/github/mdstoy/stepchart/usecase/PartialInput.java
@@ -0,0 +1,44 @@
+package com.github.mdstoy.stepchart.usecase;
+
+import com.github.mdstoy.stepchart.ArrowContainer;
+import com.github.mdstoy.stepchart.model.chart.StepChart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class PartialInput {
+
+    private ArrowContainer arrowContainer;
+
+    @Autowired
+    public PartialInput(ArrowContainer arrowContainer) {
+        this.arrowContainer = arrowContainer;
+    }
+
+    public void run(String[] args) throws FileNotFoundException {
+
+        if (args.length != 1) {
+            throw new IndexOutOfBoundsException("Required only one option is file path.");
+        }
+
+        Path path = Paths.get(args[0]);
+
+        if (Files.notExists(path)) {
+            throw new FileNotFoundException(String.format("File not found [%s]", path.toString()));
+        }
+
+        try {
+            StepChart stepChart = StepChart.of(path);
+            System.out.println(stepChart);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
