c9adb5ce8d3168e37462f08135a84b2c4e01b48d
mdstoy <toyofyou.me@gmail.com>
2018-06-11 22:20:38 +0900

    accept resolution 4 to 16

--- src/main/java/com/github/mdstoy/stepchart/model/chart/Note.java
+++ src/main/java/com/github/mdstoy/stepchart/model/chart/Note.java
@@ -9,8 +9,27 @@ public enum Note {
     OTHERS;
 
     public static Note of(int index, int resolution) {
-        // FIXME : other than quarter and eighth
-        return (index % (resolution / 4)) == 0 ? Note.QUARTER : Note.EIGHTH;
+        if (resolution == 4) {
+            return QUARTER;
+        }
+
+        if (resolution == 6) {
+            return (index % 3) == 0 ? QUARTER : TRIPLET;
+        }
+
+        if (resolution == 8) {
+            return (index % 2) == 0 ? QUARTER : EIGHTH;
+        }
+
+        if (resolution == 12) {
+            return (index % 3) == 0 ? QUARTER : TRIPLET;
+        }
+
+        if (resolution == 16) {
+            return (index % 4) == 0 ? QUARTER : (index % 2) == 0 ? EIGHTH : SIXTEENTH;
+        }
+
+        return OTHERS;
     }
 
 }
